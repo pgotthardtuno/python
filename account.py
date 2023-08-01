ffc5041ac69cc415e6259a531e040ffdc04fda93
pgotthardtuno <141185069+pgotthardtuno@users.noreply.github.com>
2023-08-01 17:23:01 -0500

    adding and fixing this lab

--- account.py
+++ account.py
@@ -1,17 +1,16 @@
-
 class Account:
-    def __init__(self,name):
+    def __init__(self, name):
         self.__account_name = name
         self.__account_balance = 0
 
-    def deposit(self,amount):
+    def deposit(self, amount):
         if float(amount) <= 0:
             return False
         else:
             self.__account_balance += float(amount)
             return True
 
-    def withdraw(self,amount):
+    def withdraw(self, amount):
         if float(amount) <= 0:
             return False
         elif float(amount) > self.__account_balance:
--- test_account.py
+++ test_account.py
@@ -6,13 +6,13 @@ from account import Account
 class TestAccount:
     self = account
     a1 = Account('John')
+
     def test___init__(self):
         assert self.a1.get_name() == 'John'
         assert self.a1.get_balance() == pytest.approx(0)
 
     def test_deposit(self):
-
-        assert self.a1.deposit(-2) is False #negative
+        assert self.a1.deposit(-2) is False  # negative
         assert self.a1.get_balance() == pytest.approx(0)
 
         assert self.a1.deposit(0) is False  # 0
@@ -21,9 +21,7 @@ class TestAccount:
         assert self.a1.deposit(2) is True  # positive
         assert self.a1.get_balance() == pytest.approx(2)
 
-
     def test_withdraw(self):
-
         assert self.a1.withdraw(-4) is False  # negative
         assert self.a1.get_balance() == pytest.approx(2)
 
